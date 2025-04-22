52dc958e5c07cb5d549424d00c00b0c1c6c13aee
josh4work0224 <josh4work0224@gmail.com>
2025-04-22 13:05:53 +0800

    Refactor HeroParticle component to enhance layout by adjusting height styling and adding flex properties for improved mobile responsiveness and visual alignment.

--- src/components/HeroParticle.js
+++ src/components/HeroParticle.js
@@ -36,7 +36,10 @@ export default function Hero() {
       style={{ height: `calc(${viewportHeight} * 100)` }}
       className="w-full flex flex-col items-center justify-center relative mx-auto overflow-hidden"
     >
-      <div className="fixed inset-0 z-10" style="height:100dvh">
+      <div
+        className="fixed inset-0 z-10 flex flex-col items-center"
+        style={{ height: `calc(${viewportHeight} * 100)` }}
+      >
         <LogoParticles particleSize={1} particleAmount={5000} />
       </div>
       {/* 使用 DecryptedText 组件显示名字 */}
